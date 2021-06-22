14358a575756b73c34279de7034743ffd2111785
harshal <harshalbisen712@gmail.com>
2021-06-22 15:09:10 +0530

    android

---
+++ app/src/main/java/com/example/traveling/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.traveling;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    LinearLayout linearLayout2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+       toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.setCheckedItem(R.id.nav_home);
+        linearLayout2 = (LinearLayout) findViewById(R.id.linearLayout2);
+
+        setSingleEvent(linearLayout2);
+    }
+
+    private void setSingleEvent(LinearLayout linearLayout2) {
+        for (int i=0;i<linearLayout2.getChildCount();i++)
+        {
+            CardView cardView = (CardView)linearLayout2.getChildAt(i);
+            final int finalI = i;
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(finalI==0)
+                    {
+                        Intent intent  = new Intent(MainActivity.this,MainActivity4.class);
+                        startActivity(intent);
+                    }
+                    else if(finalI==1)
+                    {
+                        Intent intent  = new Intent(MainActivity.this,MainActivity4.class);
+                        startActivity(intent);
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+       switch (item.getItemId()){
+           case R.id.nav_home:
+               break;
+           case R.id.nav_settings:
+               Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+               startActivity(intent);
+               break;
+           case R.id.nav_history:
+               Intent intent1 = new Intent(MainActivity.this,MainActivity3.class);
+               startActivity(intent1);
+           case R.id.nav_share:
+               Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
+               break;
+       }
+       drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
